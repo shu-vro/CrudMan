00e6f7a7db504af4159d386bd2bc3b3daaface77
Shirshen Dada <shuvra1078@gmail.com>
2022-05-28 17:35:40 +0600

    Pulled m-react-splitters in project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from "../css/App.module.scss";
 import { Nav, Config, Results } from "../components/index";
 import Wrapper from "../components/Wrapper";
-import Splitter from "m-react-splitters";
+import Splitter from "../plugins/m-react-splitters";
 import { useEffect, useState } from "react";
 
 function App() {
